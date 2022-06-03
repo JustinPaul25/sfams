e363d4ae55fe5e4e7728fec4b856238e19c26bc5
OnlyRain99 <105959339+OnlyRain99@users.noreply.github.com>
2022-06-04 07:13:20 +0800

    progresss

--- resources/js/components/students/view.vue
+++ resources/js/components/students/view.vue
@@ -42,14 +42,13 @@
                         <p>+639232323232</p>
                         <p>jane@email.com</p>
                     </dd>
+                </div>
+                <div class="py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                    <dt class="text-sm font-medium text-gray-500">Person in Notification</dt>
                     <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                        <div class="bars">
-                        <div class="info">
-                            <span>HTML</span>
-                            <span>90%</span>
-                        </div>
-                        <div class="line html"></div>
-                    </div>
+                        <p>Jane Doe</p>
+                        <p>+639232323232</p>
+                        <p>jane@email.com</p>
                     </dd>
                 </div>
                 </dl>
